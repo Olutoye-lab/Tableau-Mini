1e5ba263cd56751a35bc7b6b3e3268090e6976f7
Olutoye-lab <Olutoyeeresanmi@gmail.com>
2025-12-24 19:05:32 +0000

    Update: Agent prompt update
    Intent decoder agentic prompt are now simpler to engage consistent responses.
    
    Additioanally
    - Event data (to be pubished to the user)  is is now compiled and returned in the decode_intent method

--- SemanticCore/IntentDecoder.py
+++ SemanticCore/IntentDecoder.py
@@ -1,4 +1,5 @@
-from agentforce_setup import call_agent
+from agent import call_salesforce_agent
+from sse_manager import event_manager
 
 import json
 from dotenv import load_dotenv
@@ -10,25 +11,25 @@ class IntentDecoder:
     """
     Uses column data, metadata and user_input to decide which ontology is necessary 
     """
-    def __init__(self):
-        # In a real app, these load from a database
+    def __init__(self, user_id):
+        self.user_id = user_id
         self.ontology_library = self.get_ontology()
     
     def get_ontology(self):
         ontology = {}
 
         with open(file="ontology/Finance.json", mode="r") as finance:
-            ontology["finance"] = finance
+            ontology["finance"] = finance.read()
 
         with open(file="ontology/Sales.json", mode="r") as sales:
-            ontology["sales"] = sales
+            ontology["sales"] = sales.read()
 
         with open(file="ontology/HumanResources.json") as hr:
-            ontology["human resources"] = hr
-
+            ontology["human resources"] = hr.read()
+        
         return ontology
 
-    def decode_intent(self, user_prompt, metadata_profile):
+    def decode_intent(self, metadata_profile):
         """
         Determines which Ontology to use based on the User's goal + Data look/feel.
 
@@ -39,7 +40,6 @@ class IntentDecoder:
         Return:\n
         ontology ( string, any ){ } - A mapped ontology descibing common terms used by the firm.\n
         """
-        
         # 1. Summarize the incoming data for the AI
         # We only need column names and inferred types from Layer 1
         data_summary = {
@@ -49,35 +49,43 @@ class IntentDecoder:
 
         # 2. Construct the "System Prompt" for the LLM
         # This is where the "Innovation" happens: Prompt Engineering
-        system_prompt = f"""
-        You are a Data Architect. 
-        
-        Goal: Match the user's intent and the available data columns to the correct Data Standard.
-
-        You will be given:
-        
-        - A User Request: {user_prompt}
+        prompt = f"""
+        - A User Request: Please map analyse this data. And Return ONLY one of these three text. Human Resources, Sales or Finance
         
         - Available Data Columns: {json.dumps(data_summary)}
         
         - All Available ontology Standards:{json.dumps(self.ontology_library, indent=2)}
-        
-        Instructions:
-        1. Analyze the User Request to understand the domain (Finance vs HR vs Sales).
-        2. Look at the Data Columns to see which Standard fits best physically.
-        3. Return ONLY the text of the best matching Standard.
         """
         
         # 3. Simulate the AI Response (In prod, this is openai.ChatCompletion.create)
         # For the demo/prototype, we can simulate logic or mock the call.
         print("--- Sending Prompt to LLM ---")
-        print(system_prompt)
+        print(prompt)
         print("-----------------------------")
         
-        agent_name = os.getenv("ONTOLOGY_AGENT_NAME") or ""
+        agent_id = os.getenv("DECODER_AGENT_ID") or ""
+
         # Either finance, sales or human resources
-        ontology_name = call_agent(system_prompt, agent_name)
+        response = call_salesforce_agent(message=prompt, agent_id=agent_id)
+
+        if "Sales" in response:
+            ontology_name = "sales"
+        elif "Human Resources" in response:
+            ontology_name = "Human Resources"
+        else:
+            ontology_name = "Finance"
         
-        ontology = self.ontology_library[ontology_name.lower()]
+        ontology = json.loads(self.ontology_library[ontology_name.lower()])
+
+        fields = []
+        for item in ontology["required_fields"]:
+            fields.append({"Field": item[0], "Score": item[1]})
+
+        event_data ={
+            "id": 2,
+            "title": "Decoded Intent",
+            "text": "From the data provided, the inherited intent was semantically mapped to the corresponding ontology. Below is the mapped ontology as well as examples of fields in its data dictionary (note: the fields presented act as visaul examples, and are not directly related to fields in the provided dataset.)",
+            "table": fields
+        }
         
-        return ontology
+        return ontology, event_data
